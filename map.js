c13f866d625453dbaa3d5ed09a6e25358507ada8
Eszter Kiskarpati <keszi94@gmail.com>
2024-11-09 17:21:27 +0000

    .map() method examples

--- map.js
+++ map.js
@@ -4,26 +4,42 @@
  * command node map.js in the terminal
  */
 
-// Using a for loop
+//==..==..==..==..==..==..==..==..==..==..==..==..==..==..==..==..==..==..   Using a for loop
+let nums = [1, 2, 3, 4, 5];
+let results = [];
+for (let num of nums) {
+  results.push(num * 2);
+};
+
+console.log(results);
 
 
-// Using map()
+//==..==..==..==..==..==..==..==..==..==..==..==..==..==..==..==..==..==..   Using map()
+const multByTwo = function (num) {
+  return num * 2;
+}
+const mapResults = nums.map(multByTwo);
+console.log(mapResults);
 
 
-// Simplified w/ map()
+//==..==..==..==..==..==..==..==..==..==..==..==..==..==..==..==..==..==..   Simplified w/ map()
+const simplified = nums.map(function(num) { return num *2} );
+console.log(simplified)
 
 
-// Simplfied w/ map() + arrow function
+//==..==..==..==..==..==..==..==..==..==..==..==..==..==..==..==..==..==..   Simplfied w/ map() + arrow function
+const arrowMap = nums.map(num => num * 2);
+console.log(arrowMap);
 
 
-// With objects:
+//==..==..==..==..==..==..==..==..==..==..==..==..==..==..==..==..==..==..   With objects:
 const students = [
-  {
+  {     //<object>
     id: 1,
-    name: 'Mark',
+    name: 'Mark',              //<properties>
     profession: 'Developer',
-    skill: 'JavaScript'
-  },
+    skill: 'JavaScript'        //</properties>
+  },     //</object>
   {
     id: 2,
     name: 'Ariel',
@@ -37,3 +53,6 @@ const students = [
     skill: 'CSS'
   },
 ];
+
+const studentsWithIds = students.map(student => [student.name, student.id]); // <---  !!!!!
+console.log(studentsWithIds);
